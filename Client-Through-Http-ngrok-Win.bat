df779dd11757174515c3149f07a2c16b06a3172b
ScottyxNova <160235045+ScottyxNova@users.noreply.github.com>
2025-11-23 11:44:57 +0200

    Update Client-Through-Http-ngrok-Win.bat

--- Client-Through-Http-ngrok-Win.bat
+++ Client-Through-Http-ngrok-Win.bat
@@ -1,5 +1,5 @@
 @echo off
-set URL=https://untranslated-julee-contrite.ngrok-free.app
+set URL=https://9a40b0bac7ee.ngrok-free.app
 
 :loop
     rem Wait 0.5 seconds (approx) using ping trick
@@ -16,3 +16,4 @@ set URL=https://untranslated-julee-contrite.ngrok-free.app
     )
 
     goto loop
+
