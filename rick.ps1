7d2caecf63ec523ac9a92ef9cf508bbf567e87a5
ScottyxNova <160235045+ScottyxNova@users.noreply.github.com>
2025-11-23 15:27:51 +0200

    Update rick.ps1

--- rick.ps1
+++ rick.ps1
@@ -1,1 +1,1 @@
-start chrome.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1
+start chrome.exe 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'
